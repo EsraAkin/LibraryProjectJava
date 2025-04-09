9a41c224f7abbcde8ab389480651d9b90ba10009
Esra Akineden <e.akineden@gmail.com>
2025-04-09 13:30:06 +0200

    toparlama

---
+++ src/main/java/com/library/demo/payload/mappers/UserMappers.java
@@ -0,0 +1,56 @@
+package com.library.demo.payload.mappers;
+
+import com.library.demo.entity.user.Role;
+import com.library.demo.entity.user.User;
+import com.library.demo.payload.request.user.UserRequest;
+import com.library.demo.payload.response.user.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class UserMappers {
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    public User mapUserRequestToUser(UserRequest userRequest, Set<Role> roles){
+        return User.builder()
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .address(userRequest.getAddress())
+                .phone(userRequest.getPhone())
+                .birthDate(userRequest.getBirthDate())
+                .email(userRequest.getEmail())
+                .password(passwordEncoder.encode(userRequest.getPassword()))
+                .roles(roles)
+                .build();
+    }
+
+
+    public UserResponse mapUserToUserResponse(User user){
+        List<String> roleNames = user.getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.toList());
+
+
+        return UserResponse.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .address(user.getAddress())
+                .phone(user.getPhone())
+                .birthDate(user.getBirthDate())
+                .email(user.getEmail())
+                .roles(roleNames)
+                .build();
+
+    }
+
+}
